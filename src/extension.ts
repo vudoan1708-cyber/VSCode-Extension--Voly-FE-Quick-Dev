ed141e3b51c986c7558babeb1379781176ad89d4
Vu Doan <vu@voly.co.uk>
2023-10-04 09:30:29 +0100

    UPDATE PACKAGE.JSON && README. ABUSING SAVES ON FILES WILL NOW PRESERVE BUILT COMPONENTS UNTIL VSCODE IS SHUT DOWN OR EXT IS DEACTIVATED

--- src/extension.ts
+++ src/extension.ts
@@ -12,28 +12,42 @@ import ExpressApp from './server';
 // Constant
 import { DEV_BUILD_FOLDER, DISABLE_KEYWORD } from './constants';
 
+let rootDirectory: string | undefined;
+
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
 	const extension = new FrontendQuickDevExtension(context, new TerminalFactory());
 
 	const disposable1 = vscode.commands.registerCommand('volyfequickdev.enable', () => {
 		extension.toggleExtensionState(true);
+		vscode.window.showInformationMessage('[volyfequickdev] has been re-activated');
 	});
 	const disposable2 = vscode.commands.registerCommand('volyfequickdev.disable', () => {
 		extension.toggleExtensionState(false);
+		vscode.window.showWarningMessage('[volyfequickdev] has been deactivated');
+	});
+	const disposable3 = vscode.commands.registerCommand('volyfequickdev.remove-dev-builds-folder', () => {
+		fs.rmSync(path.join(__dirname, '..', DEV_BUILD_FOLDER), { recursive: true, force: true });
+		vscode.window.showInformationMessage('The folder has been removed');
 	});
 
-	const disposable3 = vscode.workspace.onDidSaveTextDocument(async (document: vscode.TextDocument) => {
+	const disposable4 = vscode.workspace.onDidSaveTextDocument(async (document: vscode.TextDocument) => {
 		await extension.run(document);
 	});
 
-	vscode.window.showInformationMessage('volyfequickdev is now running...');
+	vscode.window.showInformationMessage('[volyfequickdev] is now running...');
 
-	context.subscriptions.push(disposable1, disposable2, disposable3);
+	context.subscriptions.push(disposable1, disposable2, disposable3, disposable4);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	// remove existing build folder from the working workspace before VSCode is shutdown
+	if (!rootDirectory) {
+		return;
+	}
+	fs.rmSync(`${rootDirectory}/build`, { recursive: true, force: true });
+}
 
 class FrontendQuickDevExtension {
 	private _context: vscode.ExtensionContext;
@@ -54,8 +68,8 @@ class FrontendQuickDevExtension {
 	}
 
 	public async run(document: vscode.TextDocument) {
-		if (document.languageId !== 'svelte') {
-			console.warn('Not a svelte file');
+		if (document.languageId !== 'svelte' || document.fileName.includes('stories')) {
+			console.warn('Not a valid svelte component file');
 			return;
 		}
 
@@ -68,7 +82,7 @@ class FrontendQuickDevExtension {
 
 		// Check if single file disable
 		if (document.getText().includes(DISABLE_KEYWORD)) {
-			console.warn(`volyfequickdev has been disabled on ${savedFileName}`);
+			console.warn(`[volyfequickdev] has been disabled on ${savedFileName}`);
 			return;
 		}
 
@@ -85,20 +99,16 @@ class FrontendQuickDevExtension {
 			return;
 		}
 
-		vscode.window.showInformationMessage('Build completed. Locating built file(s) and making copies to the extension\'s local workspace...');
-
-		// remove existing file and folder before generating
-		fs.rmSync(path.join(__dirname, '..', DEV_BUILD_FOLDER), { recursive: true, force: true });
+		vscode.window.showInformationMessage('Build completed. Locating built file(s) and making copies of them to the extension\'s local workspace...');
 
 		// Locate the root directory
-		const fileName = vscode.window.activeTextEditor?.document.fileName;
-		const root = vscode.workspace.workspaceFolders
+		rootDirectory = vscode.workspace.workspaceFolders
 			?.map((folder) => folder.uri.fsPath)
-			?.find((fsPath) => fileName?.startsWith(fsPath));
+			?.find((fsPath) => document.fileName.startsWith(fsPath));
 
-		const devBuildsFolder = vscode.Uri.file(`${root}/build`);
+		const localBuildsFolder = vscode.Uri.file(`${rootDirectory}/build`);
 
-		await vscode.workspace.fs.copy(devBuildsFolder, vscode.Uri.file(path.join(__dirname, '..', DEV_BUILD_FOLDER)), { overwrite: true });
+		await vscode.workspace.fs.copy(localBuildsFolder, vscode.Uri.file(path.join(__dirname, '..', DEV_BUILD_FOLDER)), { overwrite: true });
 		vscode.window.showInformationMessage('The built file(s) are now fetchable via the /dev-builds endpoint...');
 	}
 

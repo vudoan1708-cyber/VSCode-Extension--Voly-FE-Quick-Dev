a2d4c3b8d58e3bf1b7d404ce88fde0b9ea275927
Vu Doan <vu@voly.co.uk>
2023-10-07 19:53:36 +0100

    CHANGE THE WAY USER ROLE IS DETERMINED TO TEST THE HYBRID CONNECTION SERVICE

--- src/azure-relay/listener.ts
+++ src/azure-relay/listener.ts
@@ -18,11 +18,17 @@ export default class RelayListener {
 
           try {
             // Session initiation
-            const parsed = JSON.parse(event.data as unknown as string);
-            this._receivedSessionId = parsed.sessionId;
-            console.log('parsed', parsed);
-          } catch {
+            if (!this._receivedSessionId) {
+              const parsed = JSON.parse(event.data as unknown as string);
+              this._receivedSessionId = parsed.sessionId;
+              console.log('parsed', parsed);
+              return;
+            }
+            throw 'Err';
+          } catch (err) {
             // Files sent
+            console.error(err);
+            console.log('Must be from the throw statement above or it is to send file');
           }
         };
         ws.on('close', () => {
--- src/user.ts
+++ src/user.ts
@@ -1,12 +1,20 @@
 import { execSync } from 'child_process';
 import { SENDERS } from './constants';
 
+import path from 'path';
+
 export default class User {
   private static _name = execSync('git config user.name', { encoding: 'utf-8' }).trim();
   private static _email = execSync('git config user.email', { encoding: 'utf-8' }).trim();
-  private static _role: 'frontend' | 'backend' = SENDERS.find((sender) => sender === User._email) ? 'frontend' : 'backend';
+  // private static _role: 'frontend' | 'backend' = SENDERS.find((sender) => sender === User._email) ? 'frontend' : 'backend';
+  // TODO: test - needs removing before commiting to git
+  private static _role: 'frontend' | 'backend' = path.basename(process.cwd()).indexOf('ui') > -1 ? 'frontend' : 'backend';
 
-  constructor() {}
+  constructor() {
+    console.log('path.basename(process.cwd())', path.basename(process.cwd()));
+    console.log('process.cwd()', process.cwd());
+    console.log('role', User._role);
+  }
 
   public get name() {
     return User._name;

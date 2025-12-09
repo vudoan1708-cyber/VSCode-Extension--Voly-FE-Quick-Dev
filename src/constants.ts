4fd64634cc6ba432688da17c2b14e874cdab8703
Vu Doan <vu@voly.com>
2025-12-09 16:13:20 +0000

    work with monorepo

--- src/constants.ts
+++ src/constants.ts
@@ -3,13 +3,22 @@ export const DEV_BUILD_FOLDER: 'dev-builds' = 'dev-builds';
 export const DISABLE_KEYWORD: 'volyfequickdev-disable' = 'volyfequickdev-disable';
 export const INCLUSTION_LIST = [
   'ui-banking',
+  'vfm-ui-banking',
   'ui-budgets',
+  'vfm-ui-budgets',
   'ui-contacts',
+  'vfm-ui-contacts',
   'ui-dashboard',
+  'vfm-ui-dashboard',
   'ui-invoices',
+  'vfm-ui-invoices',
   'ui-layout',
+  'vfm-ui-layout',
   'ui-payment-platform',
+  'vfm-ui-payment-platform',
   'ui-reports',
+  'vfm-ui-reports',
   'ui-system-config',
+  'vfm-ui-system-config',
 ];
 export const IGNOREABLE_FOLDER_NAME_RULES = /(mock).*|(data).*|(api).*|(helper).*|(test).*|(util)/i;
